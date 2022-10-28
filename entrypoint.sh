a0ecf426a4a015f6c7a4fa1b19eda3ed515f7947
Tom van den Berg <tom.vandenberg@infosupport.com>
2022-10-28 14:29:00 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 export PATH="$PATH:/root/.dotnet/tools"
 cd /github/workspace
 
-dotnet stryker --test-project Minor.Nijn.Test/Minor.Nijn.Test.csproj
+dotnet stryker --test-project Minor.Nijn.Test/Minor.Nijn.Test.csproj --project Minor.Nijn/Minor.Nijn.csproj
