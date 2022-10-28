8b52a65a4a8d015cd3e9cdae91c276b145fc4dd0
Tom van den Berg <tom.vandenberg@infosupport.com>
2022-10-28 17:21:40 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,3 @@ cd $testProject
 
 echo "Starting Striker.NET run"
 dotnet stryker
-
-htmlReport=$(cat ./StrykerOutput/**/**/*.html)
-
-echo $htmlReport
-echo "htmlReport=$htmlReport" >> $GITHUB_OUTPUT
